6f2c61e3818c0622347da02a815b65e4068be8ca
Johan Sellström <johan.sellstrom@combient.com>
2017-02-04 23:15:43 +0100

    package

--- mobile/token.go
+++ mobile/token.go
@@ -5,3 +5,12 @@ import (
         "github.com/provivus/jose/jws"
         "github.com/provivus/jose/jwt"
 )
+
+func CreateToken() {
+  // Create JWS claims
+	claims := jws.Claims{}
+	claims.SetAudience("example.com", "api.example.com")
+
+	token := jws.NewJWT(claims, crypto.SigningMethodHS256)
+	serializedToken, _ := token.Serialize([]byte("abcdef"))
+}
